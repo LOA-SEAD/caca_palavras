a4652a2c1c1aab7632511098638e1dcbcfa1d266
Luiz Valério Neto <luizvneto@gmail.com>
2017-11-28 18:16:52 -0200

    Novos créditos
    - Agora os créditos são dinâmicos e feito por base em texto e não mais imagem exportada
    - Acréscimo dos desenvolvedores do modelo de jogo
    - Acréscimo do ícone de licença CC-by-SA
    - Novo deploy

--- Deploy/offline.js
+++ Deploy/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1505838731,
+	"version": 1511900055,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -37,6 +37,7 @@
 		"images/balao3-sheet0.png",
 		"images/fade-sheet0.png",
 		"images/fundocreditos.png",
+		"images/ccbysa-sheet0.png",
 		"images/fundojogo.png",
 		"images/folheto-sheet0.png",
 		"images/folheto-sheet1.png",
